3654f26fbbb28b339defd8cb6f6933c106da2a51
iince98 <iince98@gmail.com>
2026-01-25 10:01:10 +0100

    assers

---
+++ assets/php/contact.php
@@ -0,0 +1,105 @@
+<?php 
+
+$nameErr = $emailErr = $subjectErr = $phoneErr = $messageErr = "";  
+$name = $email = $subject = $phone = $message = "";  
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    
+    //String Validation Name  
+    if (empty($_POST["name"])) {  
+         $nameErr = "Name is required. ";  
+    } else {  
+        $name = input_data($_POST["name"]);  
+        // check if name only contains letters and whitespace  
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)) {  
+           $nameErr = "Name only alphabets and white space are allowed. ";  
+        }  
+    } 
+    
+     //Phone Validation  
+    if (empty($_POST["phone"])) {  
+         $phoneErr = "Phone is required. ";  
+    } else {  
+        $phone = input_data($_POST["phone"]);  
+        // check if phone only number 
+        if (!preg_match("/^[0-9]*$/",$phone)) {  
+           $phoneErr = "Invalid phone number. ";  
+        }  
+    } 
+    
+    //Email Validation   
+    if (empty($_POST["email"])) {  
+         $emailErr = "Email is required. ";  
+    } else {  
+        $email = input_data($_POST["email"]);  
+        // check that the e-mail address is well-formed  
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  
+            $emailErr = "Invalid email format. ";  
+        }  
+    } 
+    
+  
+    $subject = "No Subject";
+  
+    
+    //String Validation Message  
+    if (empty($_POST["message"])) {  
+         $messageErr = "Message is required. ";  
+    } else {  
+        $message = input_data($_POST["message"]);  
+        // check if subject only contains letters, number and whitespace  
+        if (!preg_match("/^[a-zA-Z0-9. ]*$/",$message)) {  
+           $messageErr = "Message only alphabets, number and white space are allowed. ";  
+        }  
+    } 
+    
+    
+    if($nameErr == "" && $emailErr == "" && $subjectErr == "" && $messageErr == ""){
+        
+        $message = "<b>Mail Sender Info:</b> </br>
+            <h5><b>Name:</b> ".$name."</h5>
+            <h5><b>Email:</b> ".$email."</h5>
+            <h5><b>Phone:</b> ".$phone."</h5>
+            <h5><b>Template Name:</b> Fineco</h5>
+            </br><b>Message:</b></br>
+            <p>".$message."</p>";
+            
+    
+             $to = "info@example.com"; //Replace your real receiving email address
+            
+             $header = "From:info@example.com \r\n"; //Replace with your real web master email
+             $header .= "MIME-Version: 1.0\r\n";
+             $header .= "Content-type: text/html\r\n";
+             
+             $mail_send = mail ($to,$subject,$message,$header);
+             
+             if( $mail_send == true ) {
+                echo "Your message send successfully!.";
+             }else {
+                echo "Your message could not be send!.";
+             }
+        
+    }else{
+        
+        echo $nameErr;
+        echo $emailErr;
+        echo $subjectErr;
+        echo $messageErr;
+        
+    }
+
+}else{
+   echo "Your message could not be send!."; 
+}
+
+
+// data validate
+function input_data($data) {  
+  $data = trim($data);  
+  $data = stripslashes($data);  
+  $data = htmlspecialchars($data);  
+  return $data;  
+} 
+
+
+?>
